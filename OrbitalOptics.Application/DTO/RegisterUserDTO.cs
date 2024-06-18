fef02130dabb729360f6a17ad4346b3aade5c54c
StefanJovic3021 <95912605+StefanJovic3021@users.noreply.github.com>
2024-06-18 19:57:19 +0200

    Added OrbitalOptics.Application project

---
+++ OrbitalOptics.Application/DTO/RegisterUserDTO.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using OrbitalOptics.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrbitalOptics.Application.DTO
+{
+    public class RegisterUserDTO
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
